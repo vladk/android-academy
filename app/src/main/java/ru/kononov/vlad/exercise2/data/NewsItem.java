0fd553d7ab9fe4f08871fb88a63d38d3f67a9184
Vlad K <vladkonan@gmail.com>
2018-10-05 10:04:54 +0300

    homework 3

---
+++ app/src/main/java/ru/kononov/vlad/exercise2/data/NewsItem.java
@@ -0,0 +1,52 @@
+package ru.kononov.vlad.exercise2.data;
+
+import java.util.Date;
+
+public class NewsItem {
+
+    private final int id;
+    private final String title;
+    private final String imageUrl;
+    private final Category category;
+    private final Date publishDate;
+    private final String previewText;
+    private final String fullText;
+
+    public NewsItem(int id, String title, String imageUrl, Category category, Date publishDate, String previewText, String fullText) {
+        this.id = id;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.category = category;
+        this.publishDate = publishDate;
+        this.previewText = previewText;
+        this.fullText = fullText;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public String getPreviewText() {
+        return previewText;
+    }
+
+    public String getFullText() {
+        return fullText;
+    }
+}
